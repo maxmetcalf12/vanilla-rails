b1202f47230fd81a1903f872697082e29b4538ed
Max Metcalf <maxmetcalf12@gmail.com>
2021-03-09 13:54:54 -0800

    Remove person model

--- app/models/person.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-class Person < ApplicationRecord
-  validates :first_name, presence: true
-end
--- spec/factories/people.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :person do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    email { "MyString" }
-  end
-end
